3d910a4dfdc7041b4661e8469b2807246a28e570
Adel Mendez <adelmendez2@gmail.com>
2026-03-29 15:26:43 -0700

    update reto1
    cambie el ciclo for por un for each y agregue las demas pruebas de las actividades

--- reto1.js
+++ reto1.js
@@ -1,23 +1,30 @@
-const activities1 = [3, 1, 2, 3, 4, 2, 5]
 
 
-function preparedActivities(activities){
-    const nuevoArreglo = []
-    
-    if (activities.length === 0) {
-        console.log("No hay actividades para preparar")
-        return
-    }else{
+function preparedActivities(activities) {
+  const nuevoArreglo = [];
 
-
-    for (let i = 0; i < activities.length; i++) {
-        if (!nuevoArreglo.includes(activities[i])) {
-            nuevoArreglo.push(activities[i])
-        }
-    }
-    
-    }
-    return nuevoArreglo.sort()
+  if (activities.length === 0) {
+    console.log("No hay actividades para preparar");
+    return nuevoArreglo;
+  } else {
+    activities.forEach((element) => {
+      if (!nuevoArreglo.includes(element)) {
+        nuevoArreglo.push(element);
+      }
+    });
+  }
+  return nuevoArreglo.sort();
 }
 
-console.log(preparedActivities(activities1))
+
+const activities1 = [3, 1, 2, 3, 4, 2, 5]
+const plannedActivities1 = preparedActivities(activities1)
+console.log(plannedActivities1) // [1, 2, 3, 4, 5]
+
+const activities2 = [6, 5, 5, 5, 5]
+const plannedActivities2 = preparedActivities(activities2)
+console.log(plannedActivities2) // [5, 6]
+
+const activities3 = []
+const plannedActivities3 = preparedActivities(activities3)
+console.log(plannedActivities3)
