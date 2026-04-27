0f60d4a6e98b28ceb8394f8d2c98c31b5cb1f614
Adel Mendez <adelmendez2@gmail.com>
2026-04-27 00:07:36 -0700

    Update reto5.js

--- reto5.js
+++ reto5.js
@@ -8,10 +8,23 @@ const shoes = [
 
 function  organizeShoes(shoes) {
     
+    // Obtener las tallas únicas de los zapatos
+    // se filtra y recolecta mediante el metodo map 
+    // cada una de las tallas de los zapatos y
+    //  se utiliza el operador ... para crear un nuevo arreglo 
+    // con las tallas unicas utilizando el constructor Set que elimina
+    //  los elementos duplicados, finalmente se convierte nuevamente a
+    //  un arreglo con el operador ...
+
     const tallasUnicas = [...new Set(shoes.map(shoe => shoe.size))];
 
+    //se filtra el arreglo de tallas unicas para saber si existen
+    // tanto un pie izquierdo como un pie derecho para cada talla
     const paresDispones = tallasUnicas.filter(talla => {
 
+        //se utiliza el metodo some, este metodo se encarga 
+        // de verificar si al menos un elemento del arreglo 
+        // cumple con la condicion especificada
         const PiesIzquierdos = shoes.some(b => b.type==="I" && b.size == talla);
         const PiesDerechos = shoes.some(b => b.type==="R" && b.size == talla);
 
